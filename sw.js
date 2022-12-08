b91a0e228d196deb784106e2765d4b0327774a66
Nahuel <nahuel.profesor@gmail.com>
2022-12-08 20:57:21 -0300

    Cacheando los archivos

--- sw.js
+++ sw.js
@@ -1,7 +1,25 @@
+const nombreCache = 'apv-v1';
+// Cachear archivos
+const archivos = [
+    '/',
+    '/index.html',
+    '/css/bootstrap.css',
+    '/css/styles.css',
+    '/js/app.js',
+    '/js/apv.js'
+]
+
 // Instalar el Service Worker
 self.addEventListener('install', e => {
     console.log('Service Worker: Instalado');
-    console.log(e);
+    
+    e.waitUntil(
+        caches.open(nombreCache)
+            .then( cache => {
+                console.log('Service Worker: Cachando Archivos');
+                cache.addAll(archivos);
+            })
+    )
 })
 
 // Activar el Service worker
